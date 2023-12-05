7b6b4d78fcc1bdfdc0727c20511ef725da664b26
Illidan <Illidan@bounce.finance>
2023-12-05 16:25:20 +0800

    prepare to merge code

--- src/connectors/Web3Provider.tsx
+++ src/connectors/Web3Provider.tsx
@@ -1,20 +1,11 @@
-import { CoinbaseWallet } from '@web3-react/coinbase-wallet'
 import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core'
-import { MetaMask } from '@web3-react/metamask'
-import { WalletConnect } from '@web3-react/walletconnect'
-import { coinbaseWallet, hooks as coinbaseWalletHooks } from './coinbaseWallet'
-import { hooks as metaMaskHooks, metaMask } from './metaMask'
-import { hooks as walletConnectHooks, walletConnect } from './walletConnect'
 import React from 'react'
-import useEagerlyConnect from '../hook/hookV8/useEagerlyConnect'
-
-const connectors: [MetaMask | WalletConnect | CoinbaseWallet, Web3ReactHooks][] = [
-  [metaMask, metaMaskHooks],
-  [walletConnect, walletConnectHooks],
-  [coinbaseWallet, coinbaseWalletHooks],
-]
+// import useEagerlyConnect from '../hook/hookV8/useEagerlyConnect'
+import { connections } from '.'
+import { Connector } from '@web3-react/types'
 
 export default function Web3Provider({ children }: { children: React.ReactNode }) {
-  useEagerlyConnect()
+  const connectors = connections.map<[Connector, Web3ReactHooks]>(({ hooks, connector }) => [connector, hooks])
+  // useEagerlyConnect()
   return <Web3ReactProvider connectors={connectors}>{children}</Web3ReactProvider>
 }
